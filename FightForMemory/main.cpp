1846d547447b1b019ec0f60099c05a88eaa7314f
Hustak <hubistef16@wp.pl>
2023-07-17 04:39:50 +0200

    Dodanie odtwarzania dźwięku, który nie zachowuje się tak jak powinien

--- FightForMemory/main.cpp
+++ FightForMemory/main.cpp
@@ -3,6 +3,7 @@ extern "C" {
 #include <libavformat/avformat.h>
 }
 #include <SDL.h>
+#include <SDL_mixer.h>
 
 
 int main(int argc, char* argv[]) {
@@ -53,6 +54,27 @@ int main(int argc, char* argv[]) {
     double fps = av_q2d(pFormatCtx->streams[videoStream]->avg_frame_rate); // Œrednia liczba klatek na sekundê
     Uint32 delay = (Uint32)(1000.0 / fps); // Czas opóŸnienia miêdzy klatkami w milisekundach
 
+    // Znajduje strumieñ audio
+    int audioStream = -1;
+    for (int i = 0; i < pFormatCtx->nb_streams; i++) {
+        if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO && audioStream < 0) {
+            audioStream = i;
+            break;
+        }
+    }
+
+    // Otwiera strumieñ audio
+    AVCodecParameters* pLocalCodecParametersAudio = pFormatCtx->streams[audioStream]->codecpar;
+    const AVCodec* pCodecAudio = avcodec_find_decoder(pLocalCodecParametersAudio->codec_id);
+    AVCodecContext* pCodecCtxAudio = avcodec_alloc_context3(pCodecAudio);
+    avcodec_parameters_to_context(pCodecCtxAudio, pLocalCodecParametersAudio);
+    avcodec_open2(pCodecCtxAudio, pCodecAudio, NULL);
+
+    // Inicjuje SDL_mixer
+    Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 2048);
+    Mix_AllocateChannels(1);
+
+
 
     // Odczytuje plik
     while (av_read_frame(pFormatCtx, &packet) >= 0) {
@@ -68,6 +90,15 @@ int main(int argc, char* argv[]) {
                 SDL_RenderCopy(renderer, bmp, NULL, NULL);
                 SDL_RenderPresent(renderer);
                 SDL_Delay(delay); // OpóŸnienie synchronizuj¹ce z FPS
+            }
+        }
+        else if (packet.stream_index == audioStream) {
+            // Dekoduje audio
+            avcodec_send_packet(pCodecCtxAudio, &packet);
+            while (avcodec_receive_frame(pCodecCtxAudio, pFrame) == 0) {
+                // Odtwarza audio
+                Mix_Chunk* chunk = Mix_QuickLoad_RAW((Uint8*)pFrame->data[0], pFrame->nb_samples * 4);
+                Mix_PlayChannel(-1, chunk, 0);
             }
         }
         av_packet_unref(&packet);
